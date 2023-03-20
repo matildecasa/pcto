8743f86f7cd288fd9e95c877b7ab54806f187e1c
matildecasa <128369667+matildecasa@users.noreply.github.com>
2023-03-20 12:47:57 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float x = 3; //flag : f%
+    char e = 'E'; //flag : c%
+    char l = 'L'; //flag : c%
+    int y = 33; //flag : c%
+
+    printf("ho creato una variabile float %f, il primo carattere %c, il secondo carattere è %c, il terzo carattere è %c\n", x, e, l, y); 
+}
